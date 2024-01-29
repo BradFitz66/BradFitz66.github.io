1b42e0bb499b6a25991ad441c83279c0cda93c5c
Bradley Bath <badfitz66@gmail.com>
2024-01-29 22:04:42 +0000

    Disable SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,4 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify/functions';
 
 export default defineConfig({
-  adapter: netlify({
-    edgeMiddleware: true
-  }),
 });
